169b32bd1ce51db0897df4427f63cf98434a62b3
CHAOS2614 <330276680@qq.com>
2020-12-11 19:07:11 +0800

    spring-init

---
+++ spring-config/src/test/java/cn/edu/bjfu/springconfig/SpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjfu.springconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

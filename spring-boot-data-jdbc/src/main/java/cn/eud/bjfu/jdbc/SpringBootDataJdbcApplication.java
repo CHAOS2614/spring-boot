2b40399efd1c9c790dd06c892a2c5a9478b50284
CHAOS2614 <330276680@qq.com>
2020-12-15 16:34:33 +0800

    spring-boot-data-jdbc init

---
+++ spring-boot-data-jdbc/src/main/java/cn/eud/bjfu/jdbc/SpringBootDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package cn.eud.bjfu.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJdbcApplication.class, args);
+    }
+
+}

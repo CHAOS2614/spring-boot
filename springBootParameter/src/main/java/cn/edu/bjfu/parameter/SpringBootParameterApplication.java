ca4ecb6a632b4ee3b1dfda2d2ea777eaafd77f69
CHAOS2614 <330276680@qq.com>
2021-01-04 10:11:24 +0800

    常用参数注解使用--初始化

---
+++ springBootParameter/src/main/java/cn/edu/bjfu/parameter/SpringBootParameterApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.bjfu.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootParameterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootParameterApplication.class, args);
+    }
+
+}
---
+++ springBootParameter/src/test/java/cn/edu/bjfu/parameter/SpringBootParameterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjfu.parameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootParameterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
